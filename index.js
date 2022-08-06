9c5b37402bf88fcd21f629084ec9a3e1d31fd5a0
egor <egoclash129@gmail.com>
2022-08-06 19:25:34 +0300

    animated arrive added

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
 function doArrive(element, elementHeight, elementTop) {
-    var windowHeight = window.innerHeight;
-    elementVisible = windowHeight * 0.45;
-    if (elementTop + elementHeight > 0 && elementTop + elementHeight < elementVisible) {
-        console.log(1)
-        element.style.opacity = Math.pow((elementTop + elementHeight) / elementVisible, 2);
-      } else if ((elementTop + elementHeight >= elementVisible) && (elementTop <= windowHeight - elementVisible)) {
-        console.log(2)
-        element.style.opacity = 1;
-      } else if ((elementTop > windowHeight - elementVisible) && (elementTop < windowHeight)) {
-        console.log(3)
-        element.style.opacity = Math.pow((windowHeight - elementTop) / elementVisible, 2);
-      } else {
-        console.log(4)
-        element.style.opacity = 0;
-      }
+  var windowHeight = window.innerHeight;
+  elementVisible = windowHeight * 0.45;
+  element.style.opacity = checkPosition(elementHeight, elementTop, elementVisible)[1]
+}
+
+
+function doArriveRight(element, elementHeight, elementTop) {
+  var windowHeight = window.innerHeight;
+  elementVisible = windowHeight * 0.2;
+  positionData = checkPosition(elementHeight, elementTop, elementVisible)
+  element.style.right = positionData[2] + "%"
+}
+
+
+function doArriveLeft(element, elementHeight, elementTop) {
+  var windowHeight = window.innerHeight;
+  elementVisible = windowHeight * 0.2;
+  positionData = checkPosition(elementHeight, elementTop, elementVisible)
+  element.style.left = positionData[2] + "%"
+}
+
+
+function checkPosition(elementHeight, elementTop, elementVisible) {
+  var windowHeight = window.innerHeight;
+  if (elementTop + elementHeight > 0 && elementTop + elementHeight < elementVisible) {
+    opacity = Math.pow((elementTop + elementHeight) / elementVisible, 2);
+    shift = 100 - opacity * 100;
+    return [1, opacity, shift]
+  } else if ((elementTop + elementHeight >= elementVisible) && (elementTop <= windowHeight - elementVisible)) {
+    opacity = 1;
+    shift = 0;
+    return [2, opacity, shift]
+  } else if ((elementTop > windowHeight - elementVisible) && (elementTop < windowHeight)) {
+    opacity = Math.pow((windowHeight - elementTop) / elementVisible, 2);
+    shift = 100 - opacity * 100
+    return [3, opacity, shift]
+  } else {
+    opacity = 0;
+    shift = 100;
+    return [0, opacity, shift]
+  }
 }
 
 
 function animated() {
-    var animateds = document.querySelectorAll(".animated");
-    for (var i = 0; i < animateds.length; i++) {
-      var element = animateds[i]
-      var elementTop = element.getBoundingClientRect().top;
-      var elementHeight = element.getBoundingClientRect().height;
+    const animateds_opacity = document.querySelectorAll(".animated_opacity");
+    for (var i = 0; i < animateds_opacity.length; i++) {
+      const element = animateds_opacity[i]
+      const elementTop = element.getBoundingClientRect().top;
+      const elementHeight = element.getBoundingClientRect().height;
       window.setTimeout(doArrive, 200, element, elementHeight, elementTop);
+    }
+    const animateds_right = document.querySelectorAll(".right_arrive");
+    for (var i = 0; i < animateds_right.length; i++) {
+      const element = animateds_right[i]
+      const elementTop = element.getBoundingClientRect().top;
+      const elementHeight = element.getBoundingClientRect().height;
+      window.setTimeout(doArriveRight, 200, element, elementHeight, elementTop);
+    }
+    const animateds_left = document.querySelectorAll(".left_arrive");
+    for (var i = 0; i < animateds_left.length; i++) {
+      const element = animateds_left[i]
+      const elementTop = element.getBoundingClientRect().top;
+      const elementHeight = element.getBoundingClientRect().height;
+      window.setTimeout(doArriveLeft, 200, element, elementHeight, elementTop);
     }
     }
   
